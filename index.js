e5c9a2e15fc5f08f1cab311a594679a6aff56224
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-10-24 15:16:37 +0200

    Added a prompt for user to resize the sketchpad

--- index.js
+++ index.js
@@ -1,37 +1,72 @@
 
+// This is all for the sketch creation
 const mainContainer = document.getElementById('sketch');
-/*
-function createGrid(gridSize){
-    for(let i = 0; i < gridSize * gridSize; i++){
-        const square = document.createElement('div');
-        square.classList.add('square');
-        mainContainer.appendChild(square);
-    }
-}
-*/
 
-function createRows(container, number){
-    for(let i = 0; i < number; i++){
+function createRows(container, number) {
+    for (let i = 0; i < number; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
     }
 }
 
-function createSquares(container, number){
-    for(let i = 0; i < number; i++){
+function createSquares(container, number) {
+    for (let i = 0; i < number; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
     }
 }
 
-function createSketch(number){
+function createSketchPad(number) {
     createRows(mainContainer, number);
     const rowsCreated = document.getElementsByClassName('row');
-    for(let i = 0; i < rowsCreated.length; i++) {
+    for (let i = 0; i < rowsCreated.length; i++) {
         createSquares(rowsCreated[i], number);
     }
 }
 
-createSketch(32);
+
+createSketchPad(32);
+
+// This do the painting
+function sketchDrawing() {
+    const squares = document.getElementsByClassName('square');
+    let isDrawing = false;
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('mousedown', () => {
+            isDrawing = true;
+        });
+
+        squares[i].addEventListener('mouseup', () => {
+            isDrawing = false;
+        });
+
+        squares[i].addEventListener('mouseover', (e) => {
+            if (isDrawing) {
+                console.log("Drawing...");
+                squares[i].style = "background-color: red";
+            }
+        })
+    }
+}
+
+sketchDrawing();
+
+// Grid resizing button
+const resizeBtn = document.getElementById('resizeBtn');
+resizeBtn.addEventListener('click', () => {
+    let answer = prompt("Please enter your desired size");
+    answer = parseInt(answer);
+    if (answer > 100) {
+        answer = 100;
+    } else if (answer <= 0) {
+        answer = 1;
+    }
+    mainContainer.innerHTML = '';
+    createSketchPad(answer);
+    sketchDrawing();
+}
+)
+
